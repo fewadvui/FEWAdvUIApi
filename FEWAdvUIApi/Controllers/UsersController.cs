7420b27840549cd71eb4ac64775e3bb30aeeaa34
Jeffry Gonzalez <jeff@hypertheory.com>
2021-02-08 13:24:15 -0500

    Add project files.

---
+++ FEWAdvUIApi/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FEWAdvUIApi.Controllers
+{
+    public class UsersController : ControllerBase
+    {
+
+        [HttpHead("/users/emails/{email}")]
+        public ActionResult<bool> UserExists(string email)
+        {
+            if(email.ToLower().Trim().EndsWith("@aol.com"))
+            {
+                return Ok();
+            } else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
