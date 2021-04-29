aef0f89b5f8a7529567d3f9e9a2073403fb05e4f
Jeffry Gonzalez <jeff@hypertheory.com>
2021-04-29 11:52:26 -0400

    added items

---
+++ FEWAdvUIApi/Controllers/ProjectsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FEWAdvUIApi.Controllers
+{
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<GetItemDetails> AddItem([FromBody] PostItemRequest request)
+        {
+            // validate, etc.
+
+            var response = new GetItemDetails(
+                    new Random().Next(100,int.MaxValue),
+                    request.Item,
+                    request.Project,
+                    request.Completed,
+                    request.assignedTo
+
+                );
+
+            return StatusCode(201, response);
+        }
+    }
+
+    public record PostItemRequest( string Item,  string Project, bool Completed, string assignedTo);
+    public record GetItemDetails(int Id, string Item,  string Project, bool Completed, string assignedTo);
+
+}
+/*
+ * export interface HomeRepairModel {
+  id: string;
+  item: string;
+  project?: string;
+  completed: boolean;
+  assignedTo: string;
+}
+*/
